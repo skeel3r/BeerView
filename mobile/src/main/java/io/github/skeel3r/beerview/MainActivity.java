c480f64df53fde64afdde3cef779afbf57a07f8a
Ryan Skeele <rycoske@gmail.com>
2017-05-30 12:39:49 -0700

    Added Comments cleaned up code a bit

--- mobile/src/main/java/io/github/skeel3r/beerview/MainActivity.java
+++ mobile/src/main/java/io/github/skeel3r/beerview/MainActivity.java
@@ -15,10 +15,11 @@ import android.os.Handler;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-    TextView timeText;
-    ListView mListView;
+    // Defines
+    TextView timeText; //Text for play time
+    ListView mListView; //ListView for rules
 
-    boolean play = false;
+    boolean play = false; // Start/Stop button
     ToggleButton playPause;
 
     private long startTime = 0L;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler = new Handler();
 
     SeekBar simpleSeekBar;
+
+    // Start up Function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         timeText = (TextView) findViewById(R.id.time);
 
-
+        //Load in rules
         mListView = (ListView) findViewById(R.id.list);
         String[] values = new String[] { "Android List View",
                 "When Gandolf loves hobbits",
@@ -49,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 "Food is cooked/eaten"
         };
 
+        // Track rules for highlighting
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, values);
         mListView.setAdapter(adapter);
         mListView.setOnItemSelectedListener(new OnItemSelectedListener(){
@@ -60,9 +64,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // Start Stop Functionality
         playPause = (ToggleButton) findViewById(R.id.playPause);
-
         playPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,62 +87,68 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // initiate  views
+        // Initiate seekbar
         simpleSeekBar = (SeekBar) findViewById(R.id.simpleSeekBar);
         simpleSeekBar.incrementProgressBy(1);
         simpleSeekBar.setMax(200);
         simpleSeekBar.setProgress(0);
 
-        // perform seek bar change listener event used for getting the progress value
+        // Seek bar function listening for any changes, tracking touch and progress values
         simpleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
-                int seconds = simpleSeekBar.getProgress();
-                int mins = seconds / 60;
-                int secs = seconds % 60;
-                updateText("" + mins + ":" + String.format("%02d", secs));
+                int seconds = simpleSeekBar.getProgress(); //Progress in seconds
+                int mins = seconds / 60; // Get minutes
+                int secs = seconds % 60; // Leftover seconds
+                updateText("" + mins + ":" + String.format("%02d", secs)); // Show updated time
+
+                // Test function for highlighting listitems at desired times
                 if(secs > 25){
                     mListView.setSelection(1);
                 }
                 else{
                     mListView.setPressed(false);
                 }
-
             }
 
+            // Monitors touch movement on seekbar
             public void onStartTrackingTouch(SeekBar seekBar) {
-                handler.removeCallbacks(updateTimerThread);
-                int seconds = simpleSeekBar.getProgress();
-                int mins = seconds / 60;
-                int secs = seconds % 60;
-                updateText("" + mins + ":" + String.format("%02d", secs));
+                handler.removeCallbacks(updateTimerThread); // Stop running time thread
+                int seconds = simpleSeekBar.getProgress(); // Get new position on seekbar (seconds)
+                int mins = seconds / 60; // Convert to minutes
+                int secs = seconds % 60; // Leftover seconds
+                updateText("" + mins + ":" + String.format("%02d", secs)); // Show updated time
             }
 
+            // Determines what to do after seekbar is done being touched
             public void onStopTrackingTouch(SeekBar seekBar) {
-                timeInMilliseconds = SystemClock.uptimeMillis();
+                timeInMilliseconds = SystemClock.uptimeMillis(); // Get new time
+
+                // Only executes if continuing play
                 if(play) {
-                    progress = simpleSeekBar.getProgress()*1000;
+                    progress = simpleSeekBar.getProgress()*1000; // Set progress
                     startTime = SystemClock.uptimeMillis();
                     timeSwapBuff = 0L;
                     handler.postDelayed(updateTimerThread, 0);
                 }
+                // If user has stopped play
                 else{
                     progress = simpleSeekBar.getProgress()*1000;
-                    int seconds = simpleSeekBar.getProgress();
-                    int mins = seconds / 60;
-                    int secs = seconds % 60;
-                    updateText("" + mins + ":" + String.format("%02d", secs));
+                    int seconds = simpleSeekBar.getProgress(); // Seconds
+                    int mins = seconds / 60; // Get minutes
+                    int secs = seconds % 60; // Leftover seconds
+                    updateText("" + mins + ":" + String.format("%02d", secs)); // Show updated time
                 }
             }
 
         });
 
-
-
     }
 
+
+    // Thread for keeping track of time for the seekbar
     private Runnable updateTimerThread = new Runnable() {
 
         public void run() {
@@ -158,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    // Function for updating time display
     private void updateText(String text) {
         timeText.setText(text);
     };
